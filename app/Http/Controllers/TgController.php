c0ae8173181fd9cc8835ab674251caa1b535a74f
ibrohimxolboyev <xolboyevibrohimjon@gmail.com>
2023-02-19 22:01:55 +0500

    set webhook

--- app/Http/Controllers/TgController.php
+++ app/Http/Controllers/TgController.php
@@ -3,27 +3,57 @@
 namespace App\Http\Controllers;
 
 use Telegram\Bot\Api;
-use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Keyboard\Keyboard;
 
 class TgController extends Controller
 {
-    public function webhook()
-    {
-        $telegram = new Api('5840109465:AAFofV7YKJrmKf4S3DRstgNMROhB5xC3wAM');
-        $response = $telegram->getMe();
+        public function webhook()
+        {
+        $telegram = new Api('6191706706:AAFPTPb1P2tlD7HG3inMhG0mI3pmvTdRh4U');
+            $info = $telegram->getWebhookUpdate();
+//            $lname = $info['message']['chat']['last_name'];
+//            $fname = $info['message']['chat']['first_name'];
+            $chat_id = $info['message']['chat']['id'];
+            $text = $info['message']['text'];
+            if ($text == '/start') {
+                $keyboard = Keyboard::inlineButton(['text'=>'Отправите номер телефона','request_contact' => true]);
 
-        $botId = $response->getId();
-        $firstName = $response->getFirstName();
-        $username = $response->getUsername();
-        $params['chat_id']=787546517;
-        $params['text']="Test uchun";
-        $telegram->sendMessage($params);
-        dd($botId,$firstName,$username,$response);
+                $response = $telegram->sendMessage([
+                    'chat_id' => 'CHAT_ID',
+                    'text' => 'Hello World',
+                    'reply_markup' => $keyboard
+                ]);
+                return response()->json(['success' => 1]);
+            } elseif (isset($info['message']['entities']) && $info['message']['entities'][0]['type'] == 'phone_number') {
+                $text = $this->sendSms($text);
+            } else {
+                $text = "Botdan foydalanish uchun /start tugmasini bosing----$info";
+            }
+            $params['chat_id'] = $chat_id;
+            $params['text'] = $text;
+            $telegram->sendMessage($params);
+            return response()->json(['success' => 1]);
+        }
+        public function sendSms($phone)
+        {
+            $phone_num = substr($phone, 1, 12);
+            // $client = ApplicationCards::where([['phone',$phone_num],['is_verify',1]])->first();
+            $client = false;
+            if ($client) {
+                $code = rand(1000, 9999);
+                $mes = "N-Qulay botni faollashtirish uchun kod: $code";
+                $sms = (new SmsService())->sendSms($phone, $mes);
+                if (isset($sms['success'])) {
+                    // bazaga kodni saqlash
+                    $message = "$phone raqamiga sms kod yubordik, Iltimos kodni yuboring";
+                } else {
+                    $message = "sms yuborishda xatolik yuz berdi. Iltimos telefon raqamingizni qayta yuboring";
+                }
+            } else {
+                $message = "$phone raqamidan kredit uchun ariza berilmagan!!!";
+            }
+            return $message;
+        }
     }
 
-    public function setWebhook()
-    {
-        $response = Telegram::setWebhook(['url' => 'https://back.bdk-stock/api/webhook']);
-        dd($response);
-    }
-}
+
